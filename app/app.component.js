a189a64f7a448bee2d96c204d9fcf55efe9e181c
Robert Linden <rlinden@g.hmc.edu>
2016-09-11 21:54:33 -0700

    Split out template and CSS; generate lines iteratively from mock data initialized in OnInit using ngFor

--- app/app.component.js
+++ app/app.component.js
@@ -15,11 +15,38 @@ var AppComponent = (function () {
     }
     AppComponent.prototype.ngOnInit = function () {
         this.lines.push("MALCOLM");
+        this.lines.push("This is the sergeant");
+        this.lines.push("Who, like a good and hardy soldier, fought");
+        this.lines.push("'Gainst my captivity.--Hail, brave friend!");
+        this.lines.push("Say to the King the knowledge of the broil");
+        this.lines.push("As thou didst leave it.");
+        this.lines.push("");
+        this.lines.push("CAPTAIN");
+        this.lines.push("Doubtful it stood,");
+        this.lines.push("As two spent swimmers that do cling together");
+        this.lines.push("And choke their art.The merciless Macdonwald");
+        this.lines.push("(Worthy to be a rebel, for to that");
+        this.lines.push("The multiplying villainies of nature");
+        this.lines.push("Do swarm upon him) from the Western Isles");
+        this.lines.push("Of kerns and gallowglasses is supplied;");
+        this.lines.push("And Fortune, on his damned quarrel smiling,");
+        this.lines.push("Showed like a rebel's whore. But all's too weak;");
+        this.lines.push("For brave Macbeth(well he deserves that name),");
+        this.lines.push("Disdaining Fortune, with his brandished steel,");
+        this.lines.push("Which smoked with bloody execution,");
+        this.lines.push("Like Valor's minion, carved out his passage");
+        this.lines.push("Till he faced the slave;");
+        this.lines.push("Which ne'er shook hands, nor bade farewell to him,");
+        this.lines.push("Till he unseamed him from the nave to th' chops,");
+        this.lines.push("And fixed his head upon our battlements.");
+    };
+    AppComponent.prototype.toggleHighlightVowels = function () {
     };
     AppComponent = __decorate([
         core_1.Component({
             selector: 'my-app',
-            template: "\n    <p>\n\t{{lines[0]}} <br>\n\tThis is the sergeant <br>\n    Who, like a good and hardy soldier, fought <br>\n    'Gainst my captivity.--Hail, brave friend! <br>\n    Say to the King the knowledge of the broil <br>\n    As thou didst leave it. <br><br>\n\n    CAPTAIN <br>\n\tDoubtful it stood, <br>\n    As two spent swimmers that do cling together <br>\n    And choke their art.The merciless Macdonwald <br>\n    (Worthy to be a rebel, for to that <br>\n    The multiplying villainies of nature <br>\n    Do swarm upon him) from the Western Isles <br>\n    Of kerns and gallowglasses is supplied; <br>\n    And Fortune, on his damned quarrel smiling, <br>\n    Showed like a rebel's whore. But all's too weak; <br>\n    For brave Macbeth(well he deserves that name), <br>\n    Disdaining Fortune, with his brandished steel, <br>\n    Which smoked with bloody execution, <br>\n    Like Valor's minion, carved out his passage <br>\n    Till he faced the slave; <br>\n    Which ne'er shook hands, nor bade farewell to him, <br>\n    Till he unseamed him from the nave to th' chops, <br>\n    And fixed his head upon our battlements. <br>\n    </p>\n    "
+            templateUrl: 'app/app.component.html',
+            styleUrls: ['app/app.component.css']
         }), 
         __metadata('design:paramtypes', [])
     ], AppComponent);
--- app/app.component.ts
+++ app/app.component.ts
@@ -2,39 +2,14 @@ import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'my-app',
-  template: `
-    <p>
-	{{lines[0]}} <br>
-	This is the sergeant <br>
-    Who, like a good and hardy soldier, fought <br>
-    'Gainst my captivity.--Hail, brave friend! <br>
-    Say to the King the knowledge of the broil <br>
-    As thou didst leave it. <br><br>
-
-    CAPTAIN <br>
-	Doubtful it stood, <br>
-    As two spent swimmers that do cling together <br>
-    And choke their art.The merciless Macdonwald <br>
-    (Worthy to be a rebel, for to that <br>
-    The multiplying villainies of nature <br>
-    Do swarm upon him) from the Western Isles <br>
-    Of kerns and gallowglasses is supplied; <br>
-    And Fortune, on his damned quarrel smiling, <br>
-    Showed like a rebel's whore. But all's too weak; <br>
-    For brave Macbeth(well he deserves that name), <br>
-    Disdaining Fortune, with his brandished steel, <br>
-    Which smoked with bloody execution, <br>
-    Like Valor's minion, carved out his passage <br>
-    Till he faced the slave; <br>
-    Which ne'er shook hands, nor bade farewell to him, <br>
-    Till he unseamed him from the nave to th' chops, <br>
-    And fixed his head upon our battlements. <br>
-    </p>
-    `
+  templateUrl: 'app/app.component.html',
+  styleUrls: ['app/app.component.css']
 })
 
 export class AppComponent implements OnInit {
 	lines: string[];
+	
+	highlightVowels: boolean;
 	
 	constructor() {
 		this.lines = [];
@@ -42,5 +17,33 @@ export class AppComponent implements OnInit {
 
 	ngOnInit(): void {
 		this.lines.push("MALCOLM");
+		this.lines.push("This is the sergeant");
+		this.lines.push("Who, like a good and hardy soldier, fought");
+		this.lines.push("'Gainst my captivity.--Hail, brave friend!");
+		this.lines.push("Say to the King the knowledge of the broil");
+		this.lines.push("As thou didst leave it.");
+		this.lines.push("");
+		this.lines.push("CAPTAIN");
+		this.lines.push("Doubtful it stood,");
+		this.lines.push("As two spent swimmers that do cling together");
+		this.lines.push("And choke their art.The merciless Macdonwald");
+		this.lines.push("(Worthy to be a rebel, for to that");
+		this.lines.push("The multiplying villainies of nature");
+		this.lines.push("Do swarm upon him) from the Western Isles");
+		this.lines.push("Of kerns and gallowglasses is supplied;");
+		this.lines.push("And Fortune, on his damned quarrel smiling,");
+		this.lines.push("Showed like a rebel's whore. But all's too weak;");
+		this.lines.push("For brave Macbeth(well he deserves that name),");
+		this.lines.push("Disdaining Fortune, with his brandished steel,");
+		this.lines.push("Which smoked with bloody execution,");
+		this.lines.push("Like Valor's minion, carved out his passage");
+		this.lines.push("Till he faced the slave;");
+		this.lines.push("Which ne'er shook hands, nor bade farewell to him,");
+		this.lines.push("Till he unseamed him from the nave to th' chops,");
+		this.lines.push("And fixed his head upon our battlements.");
+	}
+	
+	toggleHighlightVowels(): void {
+		
 	}
 }
