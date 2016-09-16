1d69ff52af553c04d8cf25d3d484c049b1197aee
Sasha Friedrich <sfriedrich@hmc.edu>
2016-09-15 21:15:56 -0700

    Parsing from sample xml (cd_catalog.xml) works!

--- Project/module.js
+++ Project/module.js
@@ -21,15 +21,15 @@ angular.module('myApp',['myApp.service']);
 
 var AppController = function($scope,DataSource) {
 
-    var SOURCE_FILE = "books.xml";
+    var SOURCE_FILE = "cd_catalog.xml";
     
-    $scope.IMAGE_LOCATION = "http://rabidgadfly.com/assets/angular/xmlload/";
+    $scope.IMAGE_LOCATION = "https://scontent-lax3-1.xx.fbcdn.net/v/t1.0-9/10464370_817677894922113_3613872638784910124_n.jpg?oh=023112ffc0e1ebfb564dbae6fdaa9996&oe=58424DFD";
     
     xmlTransform = function(data) {
         console.log("transform data");
         var x2js = new X2JS();
         var json = x2js.xml_str2json( data );
-        return json.bookstore.book;
+        return json.CATALOG.CD;
     };
     
     setData = function(data) {
