ca564dcc079f755417985f227ecd996eb2bf9fc3
Sasha Friedrich <sfriedrich@hmc.edu>
2016-10-06 09:40:15 -0700

    Attempts to parse by xmlid

--- Project_copy/app.js
+++ Project_copy/app.js
@@ -5,13 +5,31 @@ App.controller('TodoCtrl', function($scope, $http) {
   	transformResponse: function(data, headersGetter) {
   		data = data.replace(/-/g, "");
   		data = data.replace(/#/g, "");
+      data = data.replace(/xml:id/g, "xmlid");
   		return angular.fromJson(data);
   	}
   })
        .then(function(data){
        		//$scope.tests = data.data.CATALOG.CD; 
-          $scope.tests = data.data.TEI.text.body.f.ab.milestone; // PhT
+          var result = data.data.TEI.text.body.f.ab.w; // PhT
+          $scope.tests = result;
+
        		//angular.fromJson(data);
           //scope.tests = data.data.TEI.text.div.p; 
-        });
+          //var result = jsonQuery('result[xmlid=w0000010]',{data:data}).value;
+          //$scope.tests=result;
+        });    
+  $scope.getWord = function(testObject){
+    return angular.fromJson($scope.tests);
+      //var newObject = angular.fromJson(testObject);
+      //if (newObject.xmlid == 'w0000010') {
+        //return "success";
+      //}
+      //return testObject.xmlid == 'w0000010';
+  }
+    //var testObject;
+    //testObject = $scope.tests;
+    //return testObject;//data.filter(
+      //function(testObject){ return testObject.xmlid == 'w0000010'}
+    
 });
