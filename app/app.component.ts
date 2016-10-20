d62f6914e0322cf134e855b94c45fc9b72527f27
kbengtsson <kbengtsson@hmc.edu>
2016-10-20 14:25:20 -0700

    adding style changes

--- app/app.component.ts
+++ app/app.component.ts
@@ -38,28 +38,28 @@ export class AppComponent implements OnInit {
 		this.scenes.push(initialScene);
 
 		let linesAct1Scene2: Line[] = [];
-		linesAct1Scene2.push(new Line("Though yet of Hamlet our dear brother's death", 10));
-		linesAct1Scene2.push(new Line("The memory be green, and that it us befitted", 13));
-		linesAct1Scene2.push(new Line("To bear our hearts in grief and our whole kingdom", 11));
-		linesAct1Scene2.push(new Line("To be contracted in one brow of woe,", 10));
-		linesAct1Scene2.push(new Line("Yet so far hath discretion fought with nature", 11));
-		linesAct1Scene2.push(new Line("That we with wisest sorrow think on him,", 10));
-		linesAct1Scene2.push(new Line("Together with remembrance of ourselves.", 10));
+		linesAct1Scene2.push(new Line("Though yet of Hamlet our dear brother's death", 10, 1));
+		linesAct1Scene2.push(new Line("The memory be green, and that it us befitted", 13, 2));
+		linesAct1Scene2.push(new Line("To bear our hearts in grief and our whole kingdom", 11, 3));
+		linesAct1Scene2.push(new Line("To be contracted in one brow of woe,", 10, 4));
+		linesAct1Scene2.push(new Line("Yet so far hath discretion fought with nature", 11, 5));
+		linesAct1Scene2.push(new Line("That we with wisest sorrow think on him,", 10, 6));
+		linesAct1Scene2.push(new Line("Together with remembrance of ourselves.", 10, 7));
 
 		this.scenes.push(new Scene(1,2, linesAct1Scene2));
 
 		let linesAct2Scene1: Line[] = [];
 
-		linesAct2Scene1.push(new Line("Marry, well said; very well said. Look you, sir,", 11));
-		linesAct2Scene1.push(new Line("Inquire me first what Danskers are in Paris;", 11));
-		linesAct2Scene1.push(new Line("And how, and who, what means, and where they keep,", 10));
-		linesAct2Scene1.push(new Line("What company, at what expense; and finding", 11));
-		linesAct2Scene1.push(new Line("By this encompassment and drift of question", 11));
-		linesAct2Scene1.push(new Line("That they do know my son, come you more nearer", 11));
-		linesAct2Scene1.push(new Line("Than your particular demands will touch it:", 11));
-		linesAct2Scene1.push(new Line("Take you, as 'twere, some distant knowledge of him;", 11));
-		linesAct2Scene1.push(new Line("As thus, 'I know his father and his friends,", 10));
-		linesAct2Scene1.push(new Line("And in part him: ' do you mark this, Reynaldo?", 11));
+		linesAct2Scene1.push(new Line("Marry, well said; very well said. Look you, sir,", 11, 1));
+		linesAct2Scene1.push(new Line("Inquire me first what Danskers are in Paris;", 11, 2));
+		linesAct2Scene1.push(new Line("And how, and who, what means, and where they keep,", 10, 3));
+		linesAct2Scene1.push(new Line("What company, at what expense; and finding", 11, 4));
+		linesAct2Scene1.push(new Line("By this encompassment and drift of question", 11, 5));
+		linesAct2Scene1.push(new Line("That they do know my son, come you more nearer", 11, 6));
+		linesAct2Scene1.push(new Line("Than your particular demands will touch it:", 11, 7));
+		linesAct2Scene1.push(new Line("Take you, as 'twere, some distant knowledge of him;", 11, 8));
+		linesAct2Scene1.push(new Line("As thus, 'I know his father and his friends,", 10, 9));
+		linesAct2Scene1.push(new Line("And in part him: ' do you mark this, Reynaldo?", 11, 10));
 
 		this.scenes.push(new Scene(2,1, linesAct2Scene1));
 	}
