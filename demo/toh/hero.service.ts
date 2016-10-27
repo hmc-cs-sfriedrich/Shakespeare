4d7648cc50c5723934c2f44a0121e6d654a16a7a
Sasha Friedrich <sfriedrich@hmc.edu>
2016-10-26 21:29:42 -0700

    demo --json import

---
+++ demo/toh/hero.service.ts
@@ -0,0 +1,41 @@
+// Observable Version
+import { Injectable }     from '@angular/core';
+import { Http, Response } from '@angular/http';
+import { Headers, RequestOptions } from '@angular/http';
+
+import { Hero }           from './hero';
+import { Observable }     from 'rxjs/Observable';
+
+@Injectable()
+export class HeroService {
+  private heroesUrl = 'app/heroes.json';  // URL to web API
+
+  constructor (private http: Http) {}
+
+  getHeroes (): Observable<Hero[]> { //Will become list of Scenes instead 
+    return this.http.get(this.heroesUrl)
+                    .map(this.extractData)
+                    .catch(this.handleError);
+  }
+
+  private extractData(res: Response) {
+    let body = res.json();
+    return body.data || { }; // Change to parse our json (from .docx) file
+  }
+
+  private handleError (error: any) {
+    // In a real world app, we might use a remote logging infrastructure
+    // We'd also dig deeper into the error to get a better message
+    let errMsg = (error.message) ? error.message :
+      error.status ? `${error.status} - ${error.statusText}` : 'Server error';
+    console.error(errMsg); // log to console instead
+    return Observable.throw(errMsg);
+  }
+}
+
+
+/*
+Copyright 2016 Google Inc. All Rights Reserved.
+Use of this source code is governed by an MIT-style license that
+can be found in the LICENSE file at http://angular.io/license
+*/
