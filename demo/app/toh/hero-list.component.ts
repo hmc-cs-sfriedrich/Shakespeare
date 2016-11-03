593502f288c189f9f54ffb6e51c761e7af18fbfb
robertmlinden <rlinden@g.hmc.edu>
2016-11-03 09:51:40 -0700

    Small Changes

--- demo/app/toh/hero-list.component.ts
+++ demo/app/toh/hero-list.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Hero }              from './hero';
 import { HeroService }       from './hero.service';
-import { Play }				       from './play';
+import { Play }				 from './play';
 import { Scene }             from './scene'
 
 @Component({
@@ -12,32 +12,19 @@ import { Scene }             from './scene'
 })
 export class HeroListComponent implements OnInit {
   errorMessage: string;
-  plays: Play;
+  plays: string;
   mode = 'Observable';
-  scene: Scene;
 
   constructor (private heroService: HeroService) {}
 
   ngOnInit() {
-    let ps: Play;
 	this.getPlay();
-	console.log("Plays is undefined?!");
-	console.log(ps === this.plays); //test
   }
 
   getPlay() {
     this.heroService.getPlay()
                      .subscribe(
-					 /*
-                       function(plays) {
-							this.plays = plays;
-							console.log(this.plays)
-							this.scene = plays[0].play.acts[0].scenes[0];
-							console.log(this.scene);
-							console.log(this.scene.sceneNumber);
-					   },
-					   */
-					   plays => this.plays = plays,
+					   play => this.play = play,
              error =>  this.errorMessage = <any>error);
   }
 }
