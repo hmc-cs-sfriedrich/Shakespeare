b7af1dab4366f485b13be080aae530656205804b
robertmlinden <rlinden@g.hmc.edu>
2016-11-29 11:08:18 -0800

    There are other things besides vowels and consonants!

--- app/app.component.ts
+++ app/app.component.ts
@@ -99,6 +99,32 @@ export class AppComponent implements OnInit {
 	}
 
 	isVowel(char: string): boolean {
-		return (char == "a") || (char == "e") || (char == "i") || (char == "o") || (char == "u");
+		let c = char.toLowerCase();
+		return (c == "a") || (c == "e") || (c == "i") || (c == "o") || (c == "u");
+	}
+
+	isConsonant(char: string): boolean {
+		let c = char.toLowerCase();
+		return (c == "b") ||
+			   (c == "c") ||
+			   (c == "d") ||
+			   (c == "f") ||
+			   (c == "g") ||
+			   (c == "h") ||
+			   (c == "j") ||
+			   (c == "k") ||
+			   (c == "l") ||
+			   (c == "m") ||
+			   (c == "n") ||
+			   (c == "p") ||
+			   (c == "q") ||
+			   (c == "r") ||
+			   (c == "s") ||
+			   (c == "t") ||
+			   (c == "v") ||
+			   (c == "w") ||
+			   (c == "x") ||
+			   (c == "y") ||
+			   (c == "z");
 	}
 }
